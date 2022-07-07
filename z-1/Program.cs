ba7d0730b5814292fab8b732a1c468a8dfc26b15
Юлька <stamina_kimo@mail.ru>
2022-07-07 15:09:22 +0300

    save

---
+++ z-1/Program.cs
@@ -0,0 +1,27 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16
+*/
+
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+int numberA = GetNumber("Введите число А: ");
+int numberB = GetNumber("Введите число В: ");
+Console.WriteLine(Math.Pow(numberA, numberB));
---
+++ z-2/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+    string errorMessage = "Вы ввели не число. Введите корректное число.";
+    while (true)
+    {
+        Console.WriteLine(message);
+        if (int.TryParse(Console.ReadLine(), out result))
+            break;
+        else
+        {
+            Console.Clear();
+            Console.WriteLine(errorMessage);
+        }
+    }
+    return result;
+}
+
+int number = GetNumber("Введите число: ");
+int sum = 0;
+for (int i = number; i > 0; i = i / 10)
+{
+    sum = sum + i % 10;
+}
+Console.WriteLine(sum);
+
